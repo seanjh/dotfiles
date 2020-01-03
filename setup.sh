94ff75bdf732e4b15f87b74666cf9f56f81bab49
Sean Herman <seanherman@gmail.com>
2020-01-02 22:54:35 -0500

    should be fine, direnv, ale tweaks

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ GIT_TARGET="$HOME/.gitconfig"
 GIT_IGNORE_TARGET="$HOME/.gitignore_global"
 TMUX_TARGET="$HOME/.tmux.conf"
 VIM_TARGET="$HOME/.vim"
+DIRENV_TARGET="$HOME/.config/direnv/direnvrc"
 
 ln -s "$SCRIPT_DIR/ackrc" "$ACK_TARGET" && echo "installed $ACK_TARGET" || echo skipping ackrc
 ln -s "$SCRIPT_DIR/gitconfig" "$GIT_TARGET" && echo "installed $GIT_TARGET" || echo skipping gitconfig
@@ -15,3 +16,4 @@ ln -s "$SCRIPT_DIR/gitignore_global" "$GIT_IGNORE_TARGET" && echo "installed $GI
 ln -s "$SCRIPT_DIR/tmux.conf" "$TMUX_TARGET" && echo "installed $TMUX_TARGET" || echo skipping tmux.conf
 ln -s "$SCRIPT_DIR/vim" "$VIM_TARGET" && echo "installed $VIM_TARGET" || echo skipping vim/
 [ ! -d "$HOME/.vim/bundle/Vundle.vim" ] && echo cloning Vundle && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || echo skipping Vundle
+mkdir -p "$HOME/.config/direnv/direnvrc"
--- vim/plugins/ale.vim
+++ vim/plugins/ale.vim
@@ -1,3 +1,6 @@
 let g:airline#extensions#ale#enabled = 1
+
+let g:ale_lint_on_text_changed = 'never'
+let g:ale_lint_on_enter = 0
 let g:ale_lint_on_insert_leave = 1
 let g:ale_fix_on_save = 1

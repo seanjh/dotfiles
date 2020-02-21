6eada386c1496833fea0e27f1888f936f9b2922d
Sean Herman <seanherman@gmail.com>
2020-02-20 21:44:35 -0500

    woops

--- setup.sh
+++ setup.sh
@@ -10,11 +10,10 @@ TMUX_TARGET="$HOME/.tmux.conf"
 VIM_TARGET="$HOME/.vim"
 DIRENV_TARGET="$HOME/.config/direnv/direnvrc"
 
-ln -sf "$SCRIPT_DIR/ackrc" "$ACK_TARGET" && echo "installed $ACK_TARGET" || echo skipping ackrc
-ln -sf "$SCRIPT_DIR/gitconfig" "$GIT_TARGET" && echo "installed $GIT_TARGET" || echo skipping gitconfig
-ln -sf "$SCRIPT_DIR/gitignore_global" "$GIT_IGNORE_TARGET" && echo "installed $GIT_IGNORE_TARGET" || echo skipping gitignore_global
-ln -sf "$SCRIPT_DIR/tmux.conf" "$TMUX_TARGET" && echo "installed $TMUX_TARGET" || echo skipping tmux.conf
-ln -sf "$SCRIPT_DIR/vim" "$VIM_TARGET" && echo "installed $VIM_TARGET" || echo skipping vim/
-[ ! -d "$HOME/.vim/bundle/Vundle.vim" ] && echo cloning Vundle && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || echo skipping Vundle
-mkdir -p "$HOME/.config/direnv/direnvrc"
-ln -sf "$SCRIPT_DIR/direnv.sh" "$HOME/.config/direnv/direnvrc"
+ln -sf "$SCRIPT_DIR/ackrc" "$ACK_TARGET" && echo "installed $ACK_TARGET" || echo ackrc error
+ln -sf "$SCRIPT_DIR/gitconfig" "$GIT_TARGET" && echo "installed $GIT_TARGET" || echo gitconfig error
+ln -sf "$SCRIPT_DIR/gitignore_global" "$GIT_IGNORE_TARGET" && echo "installed $GIT_IGNORE_TARGET" || echo gitignore_global error
+ln -sf "$SCRIPT_DIR/tmux.conf" "$TMUX_TARGET" && echo "installed $TMUX_TARGET" || echo tmux.conf error
+ln -sf "$SCRIPT_DIR/vim" "$VIM_TARGET" && echo "installed $VIM_TARGET" || echo vim/ error
+[ ! -d "$HOME/.vim/bundle/Vundle.vim" ] && echo cloning Vundle && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || echo Vundle error
+mkdir -p "$HOME/.config/direnv" && ln -sf "$SCRIPT_DIR/direnv.sh" "$HOME/.config/direnv/direnvrc" && echo installed direnvrc || echo direnvrc error

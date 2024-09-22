48cdb1dafe40f2a6e21917db3e97020fa299df64
Sean Herman <sherman1@rbi.com>
2024-09-21 21:08:29 -0400

    install mason-tool-installer

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,6 +66,7 @@ require('lazy').setup({
       -- Automatically install LSPs to stdpath for neovim
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`

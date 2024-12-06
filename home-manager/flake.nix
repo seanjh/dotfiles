2d76bff46ec685dbecd0957c027e5c55fc2fa77a
Sean Herman <sherman1@rbi.com>
2024-12-05 21:29:44 -0500

    versions

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -14,13 +14,11 @@
       donkey = let
         system = "x86_64-darwin";
       in home-manager.lib.homeManagerConfiguration {
-        username = "sean";
-        homeDirectory = "/Users/sean";
         pkgs = import nixpkgs { inherit system; };
         modules = [
-          # ./modules/shared.nix
-          # ./modules/common-darwin.nix
-          # ./modules/host-donkey.nix
+          ./modules/shared.nix
+          ./modules/common-darwin.nix
+          ./modules/host-donkey.nix
         ];
       };
 
@@ -30,8 +28,8 @@
         pkgs = import nixpkgs { inherit system; };
         modules = [
           ./modules/shared.nix
-          # ./modules/common-linux.nix
-          # ./modules/host-flipper.nix
+          ./modules/common-linux.nix
+          ./modules/host-flipper.nix
         ];
       };
     };

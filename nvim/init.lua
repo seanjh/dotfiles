5317eaa8c2781c30674123ac2e4a5d33561086cf
Sean Herman <seanherman@gmail.com>
2024-12-06 22:44:59 -0500

    2024 11 16 (#10)
    Co-authored-by: Sean Herman <sherman1@rbi.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,7 +94,7 @@ require('lazy').setup({
             formatters.black,
           },
           terraform = formatters.lsp,
-          typescript = formatters.eslint_d_fix,
+          typescript = formatters.lsp,
           typescriptreact = formatters.eslint_d_fix,
           yaml = formatters.lsp,
         },
@@ -208,6 +208,7 @@ require('lazy').setup({
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    tag = "v0.9.2",
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
@@ -473,6 +474,11 @@ local servers = {
       },
     },
   },
+  sqls = {},
+  tailwindcss = {},
+  biome = {
+    root_dir = lspconfig_util.root_pattern("tsconfig.json"),
+  },
   lua_ls = {
     settings = {
       Lua = {
@@ -481,13 +487,9 @@ local servers = {
       },
     },
   },
-  pyright = {},
-  ruff_lsp = {},
-  tailwindcss = {},
   terraformls = {},
-  ts_ls = {
-    root_dir = lspconfig_util.root_pattern("tsconfig.json"),
-  },
+  pyright = {},
+  ruff = {},
 }
 
 -- Setup neovim lua configuration
@@ -520,13 +522,14 @@ mason_lspconfig.setup_handlers {
 
 require('mason-tool-installer').setup({
   ensure_installed = {
+    "nilaway",
     "ruff",
     "nixpkgs-fmt",
-    "nilaway",
-    "oxlint",
-    "prettierd",
-    "stylelint",
-    "htmlhint",
+    "markuplint",
+    "tflint",
+    "shellcheck",
+    "editorconfig-checker",
+    "sqlfluff",
   }
 })
 

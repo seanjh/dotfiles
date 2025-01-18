ad4f0ed20c304c6d79017a862f4fd12682ec108d
Sean Herman <sherman1@rbi.com>
2025-01-18 13:19:54 -0500

    move to flipper - woops

--- home-manager/modules/host-donkey.nix
+++ home-manager/modules/host-donkey.nix
@@ -2,34 +2,4 @@
 {
   home.username = "sean";
   home.homeDirectory = "/Users/sean";
-
-  home.packages = with pkgs; [
-    (ollama.override {
-      version = "0.5.7";
-      src = fetchFromGitHub {
-        owner = "ollama";
-        repo = "ollama";
-        rev = "v${version}";
-        hash = "sha256-foobar";
-      };
-      buildInputs = [ cuda cudaPackages.cudnn ];
-    })
-    cudaPackages.cuda_nvcc
-    cudaPackages.cudnn
-  ];
-
-  systemd.user.services.ollama = {
-    Unit = {
-      Description = "Ollama AI Service";
-      After = [ "network.target" ];
-    };
-    Service = {
-      Environment = "PATH=/run/current-system/sw/bin";
-      ExecStart = "${pkgs.ollama}/bin/ollama serve";
-      Restart = "always";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
 }
--- home-manager/modules/host-flipper.nix
+++ home-manager/modules/host-flipper.nix
@@ -2,4 +2,34 @@
 {
   home.username = "sean";
   home.homeDirectory = "/home/sean";
+
+  home.packages = with pkgs; [
+    (ollama.override {
+      version = "0.5.7";
+      src = fetchFromGitHub {
+        owner = "ollama";
+        repo = "ollama";
+        rev = "v${version}";
+        hash = "sha256-foobar";
+      };
+      buildInputs = [ cuda cudaPackages.cudnn ];
+    })
+    cudaPackages.cuda_nvcc
+    cudaPackages.cudnn
+  ];
+
+  systemd.user.services.ollama = {
+    Unit = {
+      Description = "Ollama AI Service";
+      After = [ "network.target" ];
+    };
+    Service = {
+      Environment = "PATH=/run/current-system/sw/bin";
+      ExecStart = "${pkgs.ollama}/bin/ollama serve";
+      Restart = "always";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }

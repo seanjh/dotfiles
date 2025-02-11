c29a741f010d048c324ecbdb0c52e682c51f1aae
Sean Herman <seanjh@hey.com>
2025-02-11 09:23:38 -0500

    xclip

--- home-manager/modules/common-linux.nix
+++ home-manager/modules/common-linux.nix
@@ -1,8 +1,26 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 let
   baseDir = toString ./../..;
 in
 {
+  home.packages = with pkgs; [
+    xclip
+  ];
+
+  programs.bash = {
+    shellAliases = {
+      # clipboard copy
+      cbc = "xclip -selection clipboard";
+      # clipboard paste
+      cbp = "xclip -selection clipboard -o";
+    };
+  };
+
   home.file = {
     ".config/git/config_linux".source = "${baseDir}/gitconfig-linux";
   };

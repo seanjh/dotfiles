e1efe5f611de638661e8547a4c3853369a2e9ea8
Sean Herman <seanjh@hey.com>
2025-05-15 15:03:03 -0400

    alacritty

--- home-manager/hosts/host-clara.nix
+++ home-manager/hosts/host-clara.nix
@@ -1,15 +1,11 @@
 { pkgs, lib, ... }:
-let
-  alacritty-themes = pkgs.fetchFromGitHub {
-    owner = "alacritty";
-    repo = "alacritty-theme";
-    rev = "master";
-    sha256 = "sha256-n9Rvm+VqdmHyPPgkyujPm+VnWee5L4DTv6QvmEeHkUA=";
-  };
-in
 {
   home.username = "sean";
   home.homeDirectory = "/Users/sean";
+
+  imports = [
+    ../modules/programs/alacritty.nix
+  ];
 
   home.packages = with pkgs; [
     (nerdfonts.override {
@@ -59,59 +55,6 @@ in
 
   programs.bash = {
     bashrcExtra = with pkgs; lib.mkAfter '''';
-  };
-
-  programs.alacritty = {
-    enable = true;
-
-    settings = {
-      general.import = [
-        "${alacritty-themes}/themes/nord.toml"
-      ];
-      env = {
-        TERM = "xterm-256color";
-        TERM_PROGRAM = "Alacritty";
-      };
-
-      scrolling = {
-        history = 100000;
-      };
-
-      window = {
-        padding = {
-          x = 10;
-          y = 10;
-        };
-        option_as_alt = "Both";
-      };
-
-      font = {
-        normal.family = "JetBrainsMono Nerd Font";
-        bold = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Bold";
-        };
-        italic = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Italic";
-        };
-        size = 14.0;
-      };
-
-      cursor = {
-        style.blinking = "On";
-        unfocused_hollow = true;
-      };
-
-      terminal.shell = {
-        program = "${pkgs.bashInteractive}/bin/bash";
-        args = [
-          "--login"
-          "-c"
-          "tmux attach || tmux"
-        ];
-      };
-    };
   };
 
   launchd.agents = {

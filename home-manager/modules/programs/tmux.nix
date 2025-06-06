211ccf441a1c0d277e73937a9f8c9f790ea21800
Sean Herman <seanjh@hey.com>
2025-06-05 22:02:11 -0400

    fix terminal/shell and redundant settings

--- home-manager/modules/programs/tmux.nix
+++ home-manager/modules/programs/tmux.nix
@@ -2,20 +2,17 @@
 {
   programs.tmux = {
     enable = true;
-
     historyLimit = 100000;
-    # focusEvents = true;
     mouse = true;
     prefix = "C-a";
+    shortcut = "a";
     shell = "${pkgs.bashInteractive}/bin/bash";
     terminal = "tmux-256color";
+    escapeTime = 0;
+    newSession = true;
+    sensibleOnTop = false;
 
     extraConfig = ''
-      # Remap prefix from `C-b` to `C+a`
-      unbind C-b
-      set-option -g prefix C-a
-      bind-key C-a send-prefix
-
       # Config reloads
       bind r source-file ~/.config/tmux/tmux.conf
 
@@ -26,24 +23,19 @@
       bind -n M-Down select-pane -D
 
       # https://github.com/tmux/tmux/wiki/FAQ#how-do-i-use-a-256-colour-terminal
-      set -g default-terminal "tmux-256color"
       set -sa terminal-features ",xterm-256color:RGB"
 
-      set-option -g default-command "/Users/sean/.nix-profile/bin/bash --login"
-
-      # https://unix.stackexchange.com/a/109255
+      # https://unix.stackexchange.com/a/109256
       bind  c  new-window      -c "#{pane_current_path}"
       bind  %  split-window -h -c "#{pane_current_path}"
       bind '"' split-window -v -c "#{pane_current_path}"
-
-      set-option -g focus-events on
-      set-window-option -g xterm-keys on
 
       # Resolves bizarre issues with tmux muddling/misinterpreting <Esc>j and <Esc>k as Alt-j and Alt-k
       # https://github.com/tmux/tmux/wiki/Modifier-Keys#the-escape-key
       # https://github.com/LazyVim/LazyVim/discussions/163
       # https://www.reddit.com/r/lunarvim/comments/1334htt/lunarvim_in_tmux_moving_line_up_or_down_when/
-      set -sg escape-time 0
+      set-option -g focus-events on
+      set-window-option -g xterm-keys on
       set -g status-interval 0
     '';
   };

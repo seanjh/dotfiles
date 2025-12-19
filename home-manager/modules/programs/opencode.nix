d517ad97dbbe7fba277065f5612ae44d811fec21
Sean Herman <seanjh@hey.com>
2025-12-19 11:43:02 -0500

    opencode configs

--- home-manager/modules/programs/opencode.nix
+++ home-manager/modules/programs/opencode.nix
@@ -1,21 +1,31 @@
 { pkgs, ... }:
+let
+  json = pkgs.formats.json { };
+in
 {
   home.packages = with pkgs; [
     bleeding-edge.opencode
   ];
 
-  home.file = {
-    opencode-config = {
-      target = "./.config/opencode/opencode.json";
-      text = ''
-        {
-          "$schema": "https://opencode.ai/config.json",
-          "autoupdate": false,
-          "plugin": [
-            "opencode-openai-codex-auth@4.0.2"
-          ]
-        }
-      '';
+  home.file.opencode-config = {
+    target = ".config/opencode/opencode.jsonc";
+    source = json.generate "opencode.json" {
+      autoupdate = false;
+      plugin = [ "opencode-openai-codex-auth@909adb7" ];
+      mcp = {
+        context7 = {
+          type = "local";
+          enabled = true;
+          command = "npx";
+          args = [
+            "-y"
+            "@upstash/context7-mcp"
+          ];
+          environment = {
+            CONTEXT7_API_KEY = "{env:CONTEXT7_API_KEY}";
+          };
+        };
+      };
     };
   };
 }

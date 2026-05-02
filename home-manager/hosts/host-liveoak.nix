8410bdc7b6c115e2c4e0cf762a63d1ab049895d9
Sean Herman <seanjh@hey.com>
2026-05-01 22:05:25 -0400

    nvim

--- home-manager/hosts/host-liveoak.nix
+++ home-manager/hosts/host-liveoak.nix
@@ -2,4 +2,14 @@
 {
   home.username = "sean";
   home.homeDirectory = "/home/sean";
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    lua5_1
+    lua51Packages.luarocks
+    fd
+    fzf
+    lazygit
+  ];
 }
